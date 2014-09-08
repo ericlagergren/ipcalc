31a25b19cf3a5eda5b2cf71194bcfe9e96e7c4b3
Eric <ericscottlagergren@gmail.com>
2014-09-08 13:03:26 -0700

    refactor, added colors, etc.

--- ipcalc.py
+++ ipcalc.py
@@ -24,10 +24,10 @@ usage = '{0} [-n NUMHOST | -s SUBMASK] [IPv4 address/mask]'.format(prog)
 description = ('Subnetting Calculator\n=====================\nTakes both an '
                'IPv4 address and either a submask in CIDR notation,\n'
                'quad-dotted notation, or an arbitary number of hosts.'
-               '\n\nReturns a bunch of cool shit.')
+               '\n\nAddress range, host values, submasks, etc.')
 epilogue = (
     'example:\n\tipcalc 192.168.0.1/24\n\tipcalc 192.168.0.1 24\n\t'
-    'ipcalc -n 192.168.0.1 250\n\tipcalc -s 192.168.0.1 255.255.0.0')
+    'ipcalc -n 192.168.0.1 250')
 
 parser = argparse.ArgumentParser(usage=usage,
                                  description=description,
@@ -39,50 +39,62 @@ parser.add_argument(
     metavar='IPv4 address/mask',
     type=str,
     help='CIDR notation of IPv4 address\n192.168.0.1/24')
-group = parser.add_mutually_exclusive_group(required=False)
-group.add_argument(
-    '-c',
-    '--cidr',
-    metavar='CIDR num',
+parser.add_argument(
+    'prefix',
+    metavar='CIDR prefix',
+    type=str,
+    help='CIDR prefix OR submask\n24, 16, etc. OR 255.255.0.0, etc.',
+    const=None,
+    nargs='?')
+parser.add_argument(
+    '-n',
+    '--numhost',
     type=int,
-    help='CIDR number\n24, 32, 16, etc.')
-group.add_argument('-n', '--numhost', type=int, help='integer number of hosts')
-group.add_argument('-s', '--submask', type=str, help='quad-dotted submask')
+    help='integer number of hosts')
+parser.add_argument(
+    '-c',
+    '--colors',
+    action='store_true',
+    help='Colors off')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
 
-# Set initial values
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
 
-ip_input = args.ip.split('/')[0]
-hosts_input = args.numhost
-submask_input = args.submask
-
-try:
-    cidr_input = args.ip.split('/')[1]
-except IndexError:
-    cidr_input = args.cidr
-
+if False is not args.colors:
+    HEADER = OKGREEN = OKBLUE = WARNING = FAIL = ENDC = '\033[0m'
 
 MAX_BIT_INT = 32
 MAX_BIT_HEX = 0xFF
 MAX_BIT_BIN = 255
+THIRTY_TWO_BITS = 4294967295
 
 big_int = struct.Struct('!I')
 
-# Check to see if the IP address or submask are bad
 
-try:
-    IP_32_BIT = big_int.unpack(socket.inet_aton(ip_input))[0]
-except socket.error:
-    quit('Invalid IPv4 address entered.')
+def set_base(prefix, hosts):
+    """
+    Returns the 'base' which is an int which is <= 32 and >= 0
+    Represents number of 'set' bits.
 
-try:
-    SM_32_BIT = big_int.unpack(socket.inet_aton(submask_input))[0]
-except socket.error:
-    quit('Invalid submask entered.')
-except TypeError:
-    pass
+    See: https://www.google.com/search?q=hamming+weight
+
+    """
+    if None is not hosts:
+        base = int(set_bits_from_host(hosts))
+        return base
+    elif None is not prefix:
+        try:
+            base = int(prefix)
+        except ValueError:
+            base = set_bits_from_submask(prefix)
+        return base
 
 
 def set_bits_from_host(num_hosts):
@@ -104,7 +116,10 @@ def set_bits_from_submask(submask):
     """
     # [23:47] <runciter>: struct.unpack('!I', socket.inet_aton(ip))
     # from user 'runciter' in #python on freenode IRC
-    uint32_t = big_int.unpack(socket.inet_aton(submask))[0]
+    try:
+        uint32_t = big_int.unpack(socket.inet_aton(submask))[0]
+    except socket.error:
+        quit('{0}Invalid subnet mask entered.'.format(FAIL))
 
     # http://books.google.com/books?id=iBNKMspIlqEC&pg=PA66#v=onepage&q&f=false
     uint32_t = uint32_t - ((uint32_t >> 1) & 0x55555555)
@@ -115,29 +130,6 @@ def set_bits_from_submask(submask):
     return uint32_t & 0x0000003F
 
 
-def set_base(cidr, hosts, submask):
-    """
-    Returns the 'base' which is an int which is <= 32 and >= 0
-    Represents number of 'set' bits.
-
-    See: Hamming weight
-
-    """
-    if None is not cidr:
-        try:
-            base = int(cidr)
-        except ValueError:
-            quit('Don\'t include a / unless it\'s immediately followed '
-                 'by a valid CIDR prefix')
-        return base
-    elif None is not hosts:
-        base = int(set_bits_from_host(hosts))
-        return base
-    elif None is not submask:
-        base = int(set_bits_from_submask(submask))
-        return base
-
-
 def submask_from_set_bits(base):
     """
     Returns a subnetwork mask from the base
@@ -146,24 +138,15 @@ def submask_from_set_bits(base):
     try:
         mask = ~0 << (32 - base)
     except ValueError:
-        quit('CIDR prefix too large.\nMust be <= 32, >= 0.')
+        quit(
+            '{0}CIDR prefix too large.\n{1}Must be <= 32, >= 0.'.format(
+                FAIL,
+                WARNING))
     return '{0}.{1}.{2}.{3}'.format(
         mask >> 24 & MAX_BIT_BIN,
         mask >> 16 & MAX_BIT_BIN,
         mask >> 8 & MAX_BIT_BIN,
         mask & MAX_BIT_BIN)
-
-# Set our base to perform calculations
-base = set_base(cidr_input, hosts_input, submask_input)
-
-# Try setting submask round two
-# Probably could order this better but it's 2 AM and I'm tired
-try:
-    SM_32_BIT = big_int.unpack(
-        socket.inet_aton(
-            submask_from_set_bits(base)))[0]
-except socket.error:
-    quit('Invalid IPv4 address entered.')
 
 
 def wildcard(base):
@@ -196,7 +179,7 @@ def broadcast_addr(sm, ip):
     Returns broadcast address of network
 
     """
-    bn = ip | (~sm)
+    bn = ip | (~sm & THIRTY_TWO_BITS)
     # To Do: Get this gross thing to work with struct and socket
     return ".".join(map(lambda n: str(bn >> n & MAX_BIT_HEX), [24, 16, 8, 0]))
 
@@ -224,6 +207,14 @@ def display_bits():
     Prints visualization of on/off bits
 
     """
+    """
+    try:
+        b_int = bin(integer)[2:]
+        return '.'.join(b_int[i:i+8] for i in xrange(0, len(b_int), 8))
+    except TypeError:
+        return big_int.unpack(socket.inet_aton(integer))
+
+    """
     pass
 
 
@@ -232,7 +223,6 @@ def ip_class(ip):
     Returns class of IPv4 address
 
     """
-    ip = big_int.unpack(socket.inet_aton(ip))[0]
 
     if 0xC0000000 == ip & 0xC0000000:
         return 'Class C'
@@ -243,24 +233,43 @@ def ip_class(ip):
     else:
         return False
 
+# Set initial values
+
+ip_input = args.ip.split('/')[0]
+hosts_input = args.numhost
+
+try:
+    prefix_input = args.ip.split('/')[1]
+except IndexError:
+    prefix_input = args.prefix
+
+# Set base for calculations
+base = set_base(prefix_input, hosts_input)
+
+try:
+    IP_32_BIT = big_int.unpack(socket.inet_aton(ip_input))[0]
+except socket.error:
+    quit('{0}Invalid IPv4 address entered.'.format(FAIL))
+
+SM_32_BIT = big_int.unpack(socket.inet_aton(submask_from_set_bits(base)))[0]
 
 # Set values we'll be modifying
 submask = submask_from_set_bits(base)
 network_address = network_addr(SM_32_BIT, IP_32_BIT)
 broadcast_address = broadcast_addr(SM_32_BIT, IP_32_BIT)
 
-network_min = '{0}{1}'.format(
-    network_address[:-1], int(network_address[-1]) + 1)
+network_min = network_addr(SM_32_BIT + 1, IP_32_BIT)
 network_max = '{0}{1}'.format(
     broadcast_address[:-1], int(broadcast_address[-1]) - 1)
 
-print 'Address:   {0}'.format(ip_input)
-print 'Netmask:   {0} = {1}'.format(submask_from_set_bits(base), base)
-print 'Wildcard:  {0}'.format(wildcard(base))
-print '=============================='
-print 'Network:   {0}/{1}'.format(network_address, base)
-print 'HostMin:   {0}'.format(network_min)
-print 'HostMax:   {0}'.format(network_max)
-print 'Broadcast: {0}'.format(broadcast_address)
-print 'Subnets:   {0}'.format(subnets(base))
-print 'Hosts/Net: {0}\t\t\t{1}'.format(max_hosts(base), ip_class(ip_input))
+print 'Address:   {0}{1}{2}'.format(HEADER, ip_input, ENDC)
+print 'Netmask:   {0}{1} = {2}{3}'.format(HEADER, submask_from_set_bits(base), base, ENDC)
+print 'Wildcard:  {0}{1}{2}'.format(HEADER, wildcard(base), ENDC)
+print 'Class:     {0}{1}{2}'.format(HEADER, ip_class(IP_32_BIT), ENDC)
+print '{0}-->{1}'.format(OKGREEN, ENDC)
+print 'Network:   {0}{1}/{2}{3}'.format(OKBLUE, network_address, base, ENDC)
+print 'HostMin:   {0}{1}{2}'.format(OKBLUE, network_min, ENDC)
+print 'HostMax:   {0}{1}{2}'.format(OKBLUE, network_max, ENDC)
+print 'Broadcast: {0}{1}{2}'.format(OKBLUE, broadcast_address, ENDC)
+print 'Subnets:   {0}{1}{2}'.format(OKBLUE, subnets(base), ENDC)
+print 'Hosts/Net: {0}{1}{2}'.format(OKBLUE, max_hosts(base), ENDC)
